21512c7f1fab9b65f28824bc5698461c332c09f5
Andrey-Baranyuk <88671387+Andrey-Baranyuk@users.noreply.github.com>
2022-05-26 14:29:52 +0000

    Deploying to gh-pages from @ Andrey-Baranyuk/goit-react-hw-05-movies@6b74219dfeac01fb972a606b8811eb6b9b24c46d 🚀

---
+++ static/js/357.3e64805c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[357],{7357:function(e,r,t){t.r(r);var n=t(184);r.default=function(){return(0,n.jsx)("h2",{children:"Page not found error 404"})}}}]);
+//# sourceMappingURL=357.3e64805c.chunk.js.map
